ecc344663441e74d524be59ddb9157ece7aa3f9e
  <>
2022-05-25 18:03:29 +0300

    muuttujan piirtämistä jatkettu

--- nct_sdl2.c
+++ nct_sdl2.c
@@ -1,8 +1,56 @@
-#include <SDL2/SDL2.h>
+#include <SDL2/SDL.h>
+#include <netcdf.h>
 #include "nctietue.h"
 
 #define MIN(a,b) (a)<=(b)? (a): (b)
-#define GET_SCALE xlen<=mon_w && ylen<=mon_h ? 1 : MIN((float)mon_h/ylen, (float)mon_w/xlen)
+#define GET_SCALE(xlen,win_w,ylen,win_h) (xlen)<=(win_w) && (ylen)<=(win_h) ? 1 : MIN((float)(win_h)/(ylen), (float)(win_w)/(xlen))
+
+static SDL_Renderer* rend;
+static SDL_Window* window;
+static SDL_Texture* base;
+static int loop = 1;
+
+#define ONE_TYPE(a, b, ctype)						\
+  static void draw_##ctype(nct_var* var, int xid, int yid, double scale) \
+  {									\
+  double dxlen = var->dimlens[xid], dylen = var->dimlens[yid];		\
+  int xlen = var->dimlens[xid];						\
+  for(double dj=0; dj<dylen; dj+=scale) 				\
+    for(double di=0; di<dxlen; di+=scale) {				\
+      int i = di, j=dj;							\
+      int value = ((ctype*)var->data)[j*xlen + i];			\
+      SDL_SetRenderDrawColor(rend, value, value, value, 255);		\
+      SDL_RenderDrawPoint(rend, i, j);					\
+    }									\
+  }
+ALL_TYPES_EXCEPT_STRING
+#undef ONE_TYPE
+
+#define ONE_TYPE(nctype,b,ctype) [nctype]=draw_##ctype,
+void (*drawfunctions[])(nct_var*, int, int, double) = {
+  ALL_TYPES_EXCEPT_STRING
+};
+#undef ONE_TYPE
+
+void quit() {
+  SDL_DestroyTexture(base);
+  SDL_DestroyRenderer(rend);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+  loop = 0;
+}
+
+void mainloop() {
+  SDL_Event event;
+  while(loop) {
+    while(SDL_GetEvent(&event))
+      if(event.type == SDL_QUIT)
+	quit();
+    SDL_RenderCopy(rend, base);
+    SDL_RenderPresent(rend);
+    SDL_Delay(12);
+  }
+}
 
 void nct_plot_var(nct_var* var) {
   if(SDL_INIT(SDL_INIT_VIDEO)) {
@@ -10,20 +58,28 @@ void nct_plot_var(nct_var* var) {
     return;
   }
   SDL_DisplayMode dm;
-  int mon_w, mon_h;
+  int win_w, win_h;
   if(SDL_GetCurrentDisplayMode(0, &dm)) {
     fprintf(stderr, "%sError in getting monitor size%s: %s\n", error_color, SDL_GetError(), default_color);
-    mon_w = mon_h = 500;
+    win_w = win_h = 500;
   } else {
-    mon_w = dm.w;
-    mon_h = dm.h;
+    win_w = dm.w;
+    win_h = dm.h;
   }
-  int xlen = var->dimlens[var->ndims-1];
-  int ylen = var->dimlens[var->ndims-2];
-  float scale = GET_SCALE;
+  int xid = var->ndims-1, yid = var->ndims-2;
+  int xlen = var->dimlens[xid];
+  int ylen = var->dimlens[yid];
+  double scale = GET_SCALE(xlen,win_w,ylen,win_h);
   
-  SDL_Window* window = SDL_CreateWindow("Figure", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, xlen*scale, ylen*scale, SDL_WINDOW_RESIZABLE);
-  SDL_Renderer* rend = SDL_CreateRenderer(window, -1, SDL_RENDERER_TARGETTEXTURE);
+  window = SDL_CreateWindow("Figure", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, xlen*scale, ylen*scale, SDL_WINDOW_RESIZABLE);
+  rend = SDL_CreateRenderer(window, -1, SDL_RENDERER_TARGETTEXTURE);
   SDL_GetWindowSize(ikk, &ikk_w, &ikk_h); //in case window manager changed the size from requested
-  scale = GET_SCALE;
+  base = SDL_CreateTexture( rend, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, win_w, win_h );
+  scale = GET_SCALE(xlen,win_w,ylen,win_h);
+
+  SDL_SetRenderTarget(rend, base);
+  drawfunctions[var->nctype](var, xid, yid);
+  SDL_SetRenderTarget(rend, NULL);
+
+  mainloop();
 }
