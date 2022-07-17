fcaf211cb4c68e8f8d95fd0e8c160ec1683af05f
  <aerkkila@users.noreply.github.com>
2022-07-17 21:58:25 +0300

    vaihdettu makrofunktioita oikeiksi funktioiksi

---
+++ nct_functions_prototype.c
@@ -0,0 +1,83 @@
+ctype nct_varmax_##nctype(nct_var* var) {
+    return nct_varmax_anyd_##nctype(var).a.form;
+}
+
+nct_anyd nct_varmax_anyd_##nctype(nct_var* var) {
+    if(!(var->len=nct_get_varlen(var)))
+	return (nct_anyd){ {0}, -1 };
+    size_t numi=0;
+    ctype num = ((ctype*)var->data)[0];
+    for(size_t i=1; i<var->len; i++)
+	if(num < ((ctype*)var->data)[i])
+	    num = ((ctype*)var->data)[numi=i];
+    return (nct_anyd){ {.form=num}, numi };
+}
+
+ctype nct_varmin_##nctype(nct_var* var) {
+    return nct_varmin_anyd_##nctype(var).a.form;
+}
+
+nct_anyd nct_varmin_anyd_##nctype(nct_var* var) {
+    if(!(var->len=nct_get_varlen(var)))
+	return (nct_anyd){ {0}, -1 };
+    size_t numi=0;
+    ctype num = ((ctype*)var->data)[0];
+    for(size_t i=1; i<var->len; i++)
+	if(num > ((ctype*)var->data)[i])
+	    num = ((ctype*)var->data)[numi=i];
+    return (nct_anyd){ {.form=num}, numi };
+}
+
+void* nct_varminmax_##nctype(nct_var* var, void* resultv) {
+    ctype maxval, minval, *result=resultv;
+    maxval = minval = ((ctype*)var->data)[0];
+    for(int i=1; i<var->len; i++)
+	if(maxval < ((ctype*)var->data)[i])
+	    maxval = ((ctype*)var->data)[i];
+	else if(minval > ((ctype*)var->data)[i])
+	    minval = ((ctype*)var->data)[i];
+    result[0] = minval;
+    result[1] = maxval;
+    return result;
+}
+
+ctype* nct_range_##nctype(ctype i0, ctype i1, ctype gap) {
+    size_t len = (i1-i0)/gap;
+    while(i0+len*gap < i1) len++;
+    ctype* dest = malloc(len*sizeof(ctype));
+    ctype num = i0-gap;
+    for(size_t i=0; i<len; i++)
+	dest[i] = num+=gap;
+    return dest;
+}
+
+nct_var* nct_varmean_first_##nctype(nct_var* var) {
+    size_t zerolen = NCTVARDIM(*var,0).len;
+    size_t new_len = nct_get_varlen(var) / zerolen;
+    for(size_t i=0; i<new_len; i++) {
+	for(size_t j=1; j<zerolen; j++)
+	    ((ctype*)var->data)[i] += ((ctype*)var->data)[i+new_len*j];
+	((ctype*)var->data)[i] /= zerolen;
+    }
+    return nct_var_dropdim_first(var);
+}
+
+#if nctype==NC_FLOAT || nctype==NC_DOUBLE
+nct_var* nct_varmeannan_first_##nctype(nct_var* var) {
+    size_t zerolen = NCTVARDIM(*var,0).len;
+    size_t new_len = nct_get_varlen(var) / zerolen;
+    for(size_t i=0; i<new_len; i++) {
+	int count = 0;
+	ctype new_value = 0;
+	for(size_t j=0; j<zerolen; j++) {
+	    ctype test = ((ctype*)var->data)[i+new_len*j];
+	    if(test==test) {
+		count++;
+		new_value += test;
+	    }
+	}
+	((ctype*)var->data)[i] = new_value/count;
+    }
+    return nct_var_dropdim_first(var);
+}
+#endif
--- nctietue2.c
+++ nctietue2.c
@@ -7,6 +7,7 @@
 #include "internals.h"
 #include "nct_png.c"
 #include "nct_sdl2.c"
+#include "nct_functions.h"
 
 const char* nct_error_color   = "\033[1;31m";
 const char* nct_varset_color  = "\033[1;35m";
@@ -287,84 +288,21 @@ nct_any nct_varmax(nct_var* var) {
     return _varmax_anyd[var->xtype](var).a;
 }
 
-#define ONE_TYPE(nctype, idtype, ctype)				\
-    ctype nct_varmax_##nctype(nct_var* var)			\
-    {								\
-	return nct_varmax_anyd_##nctype(var).a.idtype;		\
-    }
-ALL_TYPES_EXCEPT_STRING
-#undef ONE_TYPE
-
 nct_anyd nct_varmax_anyd(nct_var* var) {
     return _varmax_anyd[var->xtype](var);
 }
-
-#define ONE_TYPE(nctype, idtype, ctype)				\
-    nct_anyd nct_varmax_anyd_##nctype(nct_var* var)		\
-    {								\
-	if(!(var->len=nct_get_varlen(var)))			\
-	    return (nct_anyd){ {0}, -1 };			\
-	size_t numi=0;						\
-	ctype num = ((ctype*)var->data)[0];			\
-	for(size_t i=1; i<var->len; i++)			\
-	    if(num < ((ctype*)var->data)[i])			\
-		num = ((ctype*)var->data)[numi=i];		\
-	return (nct_anyd){ {.idtype=num}, numi };		\
-    }
-ALL_TYPES_EXCEPT_STRING
-#undef ONE_TYPE
 
 nct_any nct_varmin(nct_var* var) {
     return _varmin_anyd[var->xtype](var).a;
 }
 
-#define ONE_TYPE(nctype, idtype, ctype)				\
-    ctype nct_varmin_##nctype(nct_var* var)			\
-    {								\
-	return nct_varmin_anyd_##nctype(var).a.idtype;		\
-    }
-ALL_TYPES_EXCEPT_STRING
-#undef ONE_TYPE
-
 nct_anyd nct_varmin_anyd(nct_var* var) {
     return _varmin_anyd[var->xtype](var);
 }
 
-#define ONE_TYPE(nctype, idtype, ctype)				\
-    nct_anyd nct_varmin_anyd_##nctype(nct_var* var)		\
-    {								\
-	if(!(var->len=nct_get_varlen(var)))			\
-	    return (nct_anyd){ {0}, -1 };			\
-	size_t numi=0;						\
-	ctype num = ((ctype*)var->data)[0];			\
-	for(size_t i=1; i<var->len; i++)			\
-	    if(num > ((ctype*)var->data)[i])			\
-		num = ((ctype*)var->data)[numi=i];		\
-	return (nct_anyd){ {.idtype=num}, numi };		\
-    }
-ALL_TYPES_EXCEPT_STRING
-#undef ONE_TYPE
-
 void* nct_varminmax(nct_var* var, void* resultv) {
     return _varminmax[var->xtype](var, resultv);
 }
-
-#define ONE_TYPE(nctype, b, ctype)				\
-    void* nct_varminmax_##nctype(nct_var* var, void* resultv)	\
-    {								\
-	ctype maxval, minval, *result=resultv;			\
-	maxval = minval = ((ctype*)var->data)[0];		\
-	for(int i=1; i<var->len; i++)				\
-	    if(maxval < ((ctype*)var->data)[i])			\
-		maxval = ((ctype*)var->data)[i];		\
-	    else if(minval > ((ctype*)var->data)[i])		\
-		minval = ((ctype*)var->data)[i];		\
-	result[0] = minval;					\
-	result[1] = maxval;					\
-	return result;						\
-    }
-ALL_TYPES_EXCEPT_STRING
-#undef ONE_TYPE
 
 nct_vset* nct_move_var_tosimilar(nct_vset* dest, nct_var* srcvar) {
     puts("this does not move attributes yet");
@@ -425,19 +363,6 @@ int nct_next_truevar_i(nct_vset* vset, int i0) {
 	    return i;
     return -1;
 }
-
-#define ONE_TYPE(nctype,form,ctype)				\
-    ctype* nct_range_##nctype(ctype i0, ctype i1, ctype gap) {	\
-	size_t len = (i1-i0)/gap;				\
-	while(i0+len*gap < i1) len++;				\
-	ctype* dest = malloc(len*sizeof(ctype));		\
-	ctype num = i0-gap;					\
-	for(size_t i=0; i<len; i++)				\
-	    dest[i] = num+=gap;					\
-	return dest;						\
-    }
-ALL_TYPES_EXCEPT_STRING
-#undef ONE_TYPE
 
 #define ONE_TYPE(nctype, form, ctype)				\
     void nct_print_##nctype(void* arr, int i, int end) {	\
@@ -601,46 +526,9 @@ nct_var* nct_varmean_first(nct_var* var) {
     return _varmean_first[var->xtype](var);
 }
 
-#define ONE_TYPE(nctype, a, ctype)					\
-    nct_var* nct_varmean_first_##nctype(nct_var* var)			\
-    {									\
-	size_t zerolen = NCTVARDIM(*var,0).len;				\
-	size_t new_len = nct_get_varlen(var) / zerolen;			\
-	for(size_t i=0; i<new_len; i++) {				\
-	    for(size_t j=1; j<zerolen; j++)				\
-		((ctype*)var->data)[i] += ((ctype*)var->data)[i+new_len*j]; \
-	    ((ctype*)var->data)[i] /= zerolen;				\
-	}								\
-	return nct_var_dropdim_first(var);				\
-    }
-ALL_TYPES_EXCEPT_STRING
-#undef ONE_TYPE
-
 nct_var* nct_varmeannan_first(nct_var* var) {
     return _varmeannan_first[var->xtype](var);
 }
-
-#define ONE_TYPE(nctype, a, ctype)				\
-    nct_var* nct_varmeannan_first_##nctype(nct_var* var)	\
-    {								\
-	size_t zerolen = NCTVARDIM(*var,0).len;			\
-	size_t new_len = nct_get_varlen(var) / zerolen;		\
-	for(size_t i=0; i<new_len; i++) {			\
-	    int count = 0;					\
-	    ctype new_value = 0;				\
-	    for(size_t j=0; j<zerolen; j++) {			\
-		ctype test = ((ctype*)var->data)[i+new_len*j];	\
-		if(test==test) {				\
-		    count++;					\
-		    new_value += test;				\
-		}						\
-	    }							\
-	    ((ctype*)var->data)[i] = new_value/count;		\
-	}							\
-	return nct_var_dropdim_first(var);			\
-    }
-ALL_TYPES_EXCEPT_STRING
-#undef ONE_TYPE
 
 nct_vset* nct_vset_isel(nct_vset* vset, int dimid, size_t ind0, size_t ind1) {
     if(ind0 > vset->dims[dimid]->len)
