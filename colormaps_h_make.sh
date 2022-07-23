54d0fd7b9c13b5a68d15794b02562ebf9c82b2b7
  <aerkkila@users.noreply.github.com>
2022-07-23 16:43:45 +0300

    echo -> printf

--- colormaps_h_make.sh
+++ colormaps_h_make.sh
@@ -2,18 +2,18 @@
 rm colormaps.h
 
 for file in `ls colormaps/colormaps_*.h`; do
-    echo "#include \"${file}\"" >> colormaps.h
+    printf "#include \"${file}\"\n" >> colormaps.h
 done
 
-echo \
+printf \
 "\n#define COLORMAP(map) cmap_##map, #map,"\
-"\nstatic char* colormaps[] = {" >> colormaps.h
+"\nstatic char* colormaps[] = {\n" >> colormaps.h
 
 for map in `ls colormaps/colormaps_*.h |grep -Po '(?<=colormaps_)\w*'`; do
-    echo "    COLORMAP(${map})" >> colormaps.h
+    printf "    COLORMAP(${map})\n" >> colormaps.h
 done
 
-echo \
+printf \
 "};\n"\
 "#undef COLORMAP\n"\
-"\n#define COLORVALUE(imap,value) (colormaps[(imap)*2] + (value)*3)" >> colormaps.h
+"\n#define COLORVALUE(imap,value) (colormaps[(imap)*2] + (value)*3)\n" >> colormaps.h
